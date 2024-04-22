15b04ff3e5635ba04525505434b898d61774b78c
Navaneethraot <youremail@gmail.com>
2024-04-22 13:04:56 +0530

    expense shell

---
+++ mysql.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$($ date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+
+echo "Script started running at :$TIMESTAMP"
+
+VALIDATE(){
+    if  [ $1 -ne 0]
+    then
+        echo "$2 ... Failure"
+        exit 1
+    else    
+        echo "$2 .....Success"
+    fi        
+}
+
+if  [  $USERID -ne 0]
+then
+    echo "Run this Script with Root Access"
+    exit  1
+else
+    echo " you are super user"
+fi        
