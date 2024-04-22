759c6394cfa3a3c1ee969a63f2c39a5b777092cc
Navaneethraot <youremail@gmail.com>
2024-04-22 18:02:58 +0530

    expense shell

--- frontend.sh
+++ frontend.sh
@@ -2,37 +2,28 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+SCRIPTNAME=$(echo $0 | cut -d '.' -f 1)
+LOGFILE="/tmp/$SCRIPTNAME-$TIMESTAMP.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[0m"
+W="\e[0m"
 
 VALIDATE(){
-    if [$1 -ne 0]
+    if [ $1 -ne 0 ]
     then
-        echo -e "$2...$R FAILURE $N"
+        echo -e "$2 ...$R FAILED $W"
     else
-        echo  -e "$2....$G SUCCESS $N"
-    fi    
+        echo -e "$2 ...$G SUCCESS $W"
+    fi
 }
-# VALIDATE(){
-#    if [ $1 -ne 0 ]
-#    then
-#         echo -e "$2...$R FAILURE $N"
-#         exit 1
-#     else
-#         echo -e "$2...$G SUCCESS $N"
-#     fi
-# }
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+    echo "Please run this script as super user"
+    exit 1
 else
-    echo "You are super user."
+    echo "Running this script as super user"
 fi
 
 dnf install nginx -y &>> $LOGFILE
