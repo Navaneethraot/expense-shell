cf894e32368ec223172586971d5cc1b36f441989
Navaneethraot <youremail@gmail.com>
2024-04-22 13:11:00 +0530

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -4,23 +4,32 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
-echo "Script started running at :$TIMESTAMP"
+echo "Scrippt started running at : $TIMESTAMP"
+
 
 VALIDATE(){
-    if  [ $1 -ne 0]
-    then
-        echo "$2 ... Failure"
+
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
         exit 1
-    else    
-        echo "$2 .....Success"
-    fi        
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
 }
 
 if  [ $USERID -ne 0 ]
-then
-    echo "Run this Script with Root Access"
-    exit  1
-else
-    echo " you are super user"
-fi        
+    then
+        echo "Please run this script with root access"
+        exit 1
+    else
+        echo "you are super user"
+    fi  
+
+    dnf install mysql -y &>>$LOGFILE
+    VALIDATE $? " Installing mysql server"
