fa1dc799c38c735b1ab4ebc92bf05642a048ee44
Navaneethraot <youremail@gmail.com>
2024-04-22 16:42:35 +0530

    expense shell

--- backend.sh
+++ backend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
@@ -7,6 +8,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "Please enter DB password:"
+read -s mysql_root_password
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -25,8 +28,9 @@ then
 else
     echo "You are super user."
 fi
+
 dnf module disable nodejs -y &>>$LOGFILE
-VALIDATE $? "Disabling Nodejs"
+VALIDATE $? "Disabling default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOGFILE
 VALIDATE $? "Enabling nodejs:20 version"
@@ -34,18 +38,19 @@ VALIDATE $? "Enabling nodejs:20 version"
 dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing nodejs"
 
-id expense
-if [ $? --ne 0]
+id expense &>>$LOGFILE
+if [ $? -ne 0 ]
 then
-useradd expense &>>$LOGFILE
-VALIDATE $? "Creating User expense"
+    useradd expense &>>$LOGFILE
+    VALIDATE $? "Creating expense user"
 else
-echo -e "Expense user already created...$Y SKIPPING $N"
+    echo -e "Expense user already created...$Y SKIPPING $N"
 fi
+
 mkdir -p /app &>>$LOGFILE
 VALIDATE $? "Creating app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
 VALIDATE $? "Downloading backend code"
 
 cd /app
@@ -56,6 +61,7 @@ VALIDATE $? "Extracted backend code"
 npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependencies"
 
+#check your repo and path
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "Copied backend service"
 
@@ -76,4 +82,3 @@ VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
 VALIDATE $? "Restarting Backend"
-
